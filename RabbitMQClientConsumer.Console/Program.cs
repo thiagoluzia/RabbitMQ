904141cbdf70a53d83a7c7e47e4fc93c3b37bb0a
thiagomoura <thiago.mouraluzia@gmail.com>
2024-05-24 20:58:30 -0300

    Consumer com RabitMQ.Cliente

--- RabbitMQClientConsumer.Console/Program.cs
+++ RabbitMQClientConsumer.Console/Program.cs
@@ -2,11 +2,13 @@
 using RabbitMQ.Client.Events;
 using System.Text;
 using System.Text.Json;
+using System;
 
 public class Program
 {
     private static void Main(string[] args)
     {
+        Console.WriteLine("Inicio");
         //Criar uma fila
         const string QUEUE = "praticando-rabbit-mq-fila";
 
@@ -26,8 +28,9 @@ public class Program
         var consumer = new EventingBasicConsumer(canalConsumer);
 
         //Consumidor recebe e processa uma mensagem recebida
-        consumer.Received += async (sender, eventArgs) =>
+        consumer.Received += (sender, eventArgs) =>
         {
+            #region implementação pessoal
             // 1. Obter o array de bytes da mensagem:
             var contentArray = eventArgs.Body.ToArray();
 
@@ -40,16 +43,25 @@ public class Program
             Console.WriteLine($"Mensagem recebida: {contentString}");
 
             // 4. Confirmar o recebimento da mensagem (ACK):
-            canalConsumer.BasicAck(eventArgs.DeliveryTag, false);
-            
+            canalConsumer.BasicAck(eventArgs.DeliveryTag,false);
+            #endregion
         };
 
-        canalConsumer.BasicConsume(QUEUE, false, consumer);
+        canalConsumer.BasicConsume(QUEUE,false, consumer);
 
+
+        Thread.Sleep(TimeSpan.FromSeconds(1));
     }
 
     public class Pessoa
     {
+        public Pessoa(int id, string? nome, string? sobrenome)
+        {
+            Id = id;
+            Nome = nome;
+            Sobrenome = sobrenome;
+        }
+
         public int Id { get; set; }
         public string?  Nome { get; set; }
         public string? Sobrenome { get; set; }
